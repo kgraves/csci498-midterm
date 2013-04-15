eed04ce38fc7d93eb09c0f8578156a56714ceb78
kgraves <kgraves9@mail.csuchico.edu>
2013-04-14 21:53:50 -0700

    adds update and destroy features

--- src/app.js
+++ src/app.js
@@ -7,27 +7,6 @@ $(document).ready( function() {
     schemaString: 'id INTEGER PRIMARY KEY, ' + 'login TEXT, ' + 'url TEXT'
   };
 
-  /*
-  var dbSchema = {
-    schemaString: 'avatar_url TEXT, ' +
-      'events_url TEXT, ' +
-      'followers_url TEXT, ' +
-      'following_url TEXT, ' +
-      'gists_url TEXT, ' +
-      'gravatar_id TEXT, ' +
-      'html_url TEXT, ' +
-      'id INTEGER PRIMARY KEY, ' +
-      'login TEXT, ' +
-      'organizations_url TEXT, ' +
-      'received_events_url TEXT, ' +
-      'repos_url TEXT, ' +
-      'starred_url TEXT, ' +
-      'subscriptions_url TEXT, ' +
-      'type TEXT, ' +
-      'url TEXT'
-  };
-  */
-
   // ############ MODELS ############
 
   var User = Backbone.Model.extend({
@@ -53,78 +32,110 @@ $(document).ready( function() {
       // initial data fetch
       this.collection.fetch({
         success: function(collection, response){
+
           // insert models into db
           _.each(collection.models, function(model) {
             db.insert('users', {id: model.attributes.id, 
                                 login: model.attributes.login, 
                                 url: model.attributes.url});
+
+            $('#users_tbody').append('<tr id="model">' +
+                                        '<td>' + model.attributes.id + '</td>' +
+                                        '<td>' + model.attributes.login + '</td>' +
+                                        '<td>' + model.attributes.url + '</td>' +
+                                        '<td id="destroy"><p>Destroy</p></td>' +
+                                     '</tr>');
           });
 
           // initial render
-          App.render();
+          // App.render();
         },
         failure: function() {
-          console.log('FAIL');
         }
       });
     },
 
     render: function() {
+
       // select all users
       db.select('users', '*', null, null, function(results) {
+
+        // clear tbody
+        $('#users_tbody tr').each( function() {
+          $(this).remove();
+        });
+
         for(x=0; x<results.rows.length; x++) {
           model = new User(results.rows.item(x));
           App.collection.push(model);
 
-          // render model to ui
-          $('#main_content').append('<tr id="model">' +
+          $('#users_tbody').append('<tr id="model">' +
                                       '<td>' + model.attributes.id + '</td>' +
                                       '<td>' + model.attributes.login + '</td>' +
                                       '<td>' + model.attributes.url + '</td>' +
-                                    '</tr>');
+                                      '<td id="destroy"><p>Destroy</p></td>' +
+                                   '</tr>');
         };
-      });
 
-      /*
-      _.each(this.collection.models, function(model) {
-
-        // render model to ui
-        $('#main_content').append('<tr id="model">' +
-                                    '<td>' + model.attributes.id + '</td>' +
-                                    '<td>' + model.attributes.login + '</td>' +
-                                    '<td>' + model.attributes.url + '</td>' +
-                                  '</tr>');
       });
-      */
 
       return this;
     }      
   });
 
-  // ############ APP ############
+  // ############ UTILS ############
 
   // form submit handler
-  //    creates a new user model and 
-  //    adds it to the App
   $('#user_form').submit( function() {
+    user_id = $('#user_id').val();
     user_login = $('#user_login').val();
     user_url = $('#user_url').val();
 
-    // insert and render ui
-    db.insert('users', {login: user_login, url: user_url});
+    // hidden field data determines whether update or insert
+    if (user_id == "") {
+      db.insert('users', {login: user_login, url: user_url});
+    }
+    else {
+      db.update('users', {login: user_login, url: user_url}, {id: user_id});
+    }
+
     App.render();
 
     // clear ui elements
+    $('#user_id').val('');
     $('#user_login').val('');
     $('#user_url').val('');
 
     return false;
   });
 
+  // click handler for table row tags
+  $(document).on("click", '#model', function() {
+    // populate user form fields
+    $('#user_id').val( $(this).context.cells[0].textContent );
+    $('#user_login').val( $(this).context.cells[1].textContent );
+    $('#user_url').val( $(this).context.cells[2].textContent );
+  });
+
+  // click handler for remove paragraph tags
+  $(document).on("dblclick", '#destroy', function() {
+    user_id = $(this).parent('tr').get(0).cells[0].textContent;
+
+    db.destroy('users', {id: user_id});
+    $(this).parent().remove();
+
+    // clear ui elements
+    $('#user_id').val('');
+    $('#user_login').val('');
+    $('#user_url').val('');
+  });
+
+  // ############ APP ############
+
   // try dropping then creating table
   db.dropTable('users');
   db.createTable('users', dbSchema.schemaString);
 
-  var App = new AppView({el: $('#main_content')});
+  var App = new AppView({el: $('#users_tbody')});
 
 });
